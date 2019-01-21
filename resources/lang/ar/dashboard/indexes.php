35311f10a76e3128d96032407dbfa3a24dad60a6
Hamza Zymawy <zymawy@gmail.com>
2019-01-21 04:03:03 +0300

    Transalate Indexes

---
+++ resources/lang/ar/dashboard/indexes.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ironside
+ * Date: 12/8/18
+ * Time: 9:55 PM
+ */
+
+return [
+    // Banners
+    'list_all_banners' => 'قائمة جميع لافتات',
+    'list_all_articles' => 'قائمة جميع المقالات',
+    'list_all_articles_cat' => 'قائمة جميع أقسام المقالات',
+    'list_all_document' => 'قائمة جميع المستندات',
+    'list_all_document_cat' => 'قائمة جميع أقسام المستندات',
+    'list_all_dashboard_action' => 'قائمة جميع إجراءات لوحة التحكم',
+    'list_all_website_action' => 'قائمة جميع الإجراءات موقع',
+    'list_all_clients' => 'قائمة العملاء',
+    'list_all_news_cat' => 'قائمة جميع أقسام الأخبار',
+    'list_all_roles' => 'قائمة جميع الأدوار',
+    'list_all_permissions' => 'قائمة جميع الأذونات',
+    'list_all_settings' => 'قائمة جميع الإعدادات',
+    'list_all_countries' => 'قائمة جميع الدول',
+    'list_all_cities' => 'قائمة جميع المدن',
+    'list_all_provinces' => 'قائمة جميع المحافظات',
+    'list_all_suburbs' => 'قائمة كل الضواحي',
+    'list_all_adminstrators' => 'جميع المدراء المدعويين',
+    'list_all_newsletters' => 'قائمة جميع المشتركين في النشرة الإخبارية',
+    'list_all_news' => 'قائمة كل الأخبار',
+    'banner' => 'بنر',
+    'articles' => 'مقالات',
+    'document' => 'مستند',
+    'summary' => 'ملخص',
+    'button' => 'زر',
+    'image' => 'صورة',
+    'website' => 'موقع',
+
+    // Document
+    'total_document' => 'إجمالي المستندات',
+    'add_document_to' => 'أضف المستندات إلى',
+
+    //User
+
+    'user' => 'المستخدم',
+    'invite_admin_test' => 'دعوة مسؤول',
+    'backup_tool' => 'نسخة احتياطية ',
+    'invite_admin_test_inside' => 'دعوة مسؤول',
+    'email_to_send_text' => 'عنوان البريد الإلكتروني للمستخدم الذي تريد دعوته',
+    'last_login' => 'آخر تسجيل دخول',
+    'send_invite' => 'ارسل دعوة',
+    'impersonate' => 'انتحال ',
+    'invited_by' => 'بدعوة من',
+    'confirmed' => 'تم تأكيد ',
+    'confirm' => 'تأكيد ',
+    'claimed_on' => 'اخذه في  ',
+    'confirmed_yat' => 'غير مؤكد بعد ',
+    'full_name' => 'الاسم الكامل',
+    'full_name_placeholder' => 'يرجى إدخال الاسم الكامل',
+    'cell_phone' => 'الهاتف الخلوي',
+    'telephone' => 'هاتف',
+    'phone' => 'هاتف',
+    'email' => 'البريد الالكتروني ',
+    'roles' => 'الادوار ',
+    'show' => 'عرض ',
+    'edit' => 'تعديل ',
+    'delete' => 'حذف ',
+    'account_holder' => 'صاحب الحساب ',
+    'createdAt' => 'تاريخ الاضافة',
+    'registeredAt' => 'مسجل فى',
+    'confirmed_at' => 'وأكد في',
+    'user_confirmed' => 'أكد المستخدم',
+    'confirmed_their' => ' تم تأكيد الحساب',
+
+    'action' => 'اجراء',
+    'after' => 'بعد',
+    'before' => 'قبل',
+    'created' => 'تم الاضافة',
+    'belongs_to' => 'ينتمى الى ',
+
+    // Photos
+    'list_all_photos' => 'عرض جميع ألبومات الصور',
+    'name' => 'الاسم',
+    'total_photos' => 'مجموع الصور',
+    'cover_photo' => 'غلاف الصورة',
+    'add_photo' => 'اضافة صورة الى  ',
+    'photos' => 'الصور',
+    'photo_instruct' => 'يرجى النقر على اللوحة أدناه لتحميل الصور إلى ',
+    'upload_photo' => 'حمل الصورة',
+    'photo_howIt_work' => 'كيف يعمل؟',
+    'photo_howIt_work_info'=> "انقر على الزر أدناه للتصفح بحثًا عن الصور <br/>
+                         قم بتحديث الصفحة عند اكتمال التحميل <br/>
+                         انقر على اسم الصورة لتغييرها <br/>
+                         انقر فوق زر الراديو لتعيين صورة الغلاف <br/>",
+    'photo_click_here' => 'انقر هنا لتصفح الصور.',
+    'update_photo_name' => 'تحديث اسم الصورة',
+    'name_of_photo' => 'اسم الصورة',
+    'name_of_photo_placeholder' => 'أدخل اسم الصورة',
+    'msg_photo_uploaded' => 'تم تحميل الصورة وحفظها في قاعدة البيانات.',
+    'covered_photo_uploaded' => 'تم تحديث صورة الغلاف.',
+    'photo_uploaded' => 'تم تحديث اسم الصورة.',
+    'photo_album' => 'إلبوم الصور',
+    'photo_album_placeholder' => 'اسم ألبوم الصور',
+
+    // Crop
+
+    'crop' => 'اقتصاص ',
+    'current_thumb' => 'الإبهام الحالي',
+    'preview' => 'معاينة',
+    'image_size' => 'حجم الصورة',
+    'image_size_info' => 'الصورة التي تم تحميلها أصغر من الحد الأدنى للحجم المطلوب.',
+    'cropped' => 'تم الاقتصاص',
+    'cropped_success_msg' => 'تم اقتصاص الصورة بنجاح.',
+    'delete_photo' => 'حذف الصورة - ',
+    'cover' => 'التغطية',
+
+    // pagination
+
+    'filtered_from' => 'تصفية من ',
+    'total_entries' => 'مجموع المدخلات',
+
+    // Locations
+    'latitude' => 'خط العرض',
+    'longitude' => 'خط الطول',
+
+    'countries' => 'الدول',
+    'country' => 'الدولة',
+    'country_placeholder' => 'رجاء قم بإختيار دوله',
+    'cities' => 'المدن',
+    'city' => 'المدينة',
+    'city_placeholder' => 'يرجى اختيار المدينة',
+    'provinces' => 'المحافظات',
+    'suburbs' => 'الضواحي',
+    'subscribed_on' => 'مشترك في',
+    'keyword' => 'الكلمة',
+];
