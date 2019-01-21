1ca549187efea577c839300c125e6a0c5cd790d3
Hamza Zymawy <zymawy@gmail.com>
2019-01-21 04:15:40 +0300

    Transalate LogManager

---
+++ resources/lang/ar/dashboard/log.php
@@ -0,0 +1,30 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | LogManager
+    |--------------------------------------------------------------------------
+    */
+    'log_manager'                 => 'مدير السجل',
+    'log_manager_description'     => 'معاينة ، تنزيل وحذف السجلات.',
+    'actions'                     => 'أجراءات',
+    'delete_confirm'              => 'هل أنت متأكد من أنك تريد حذف ملف السجل هذا؟',
+    'back_to_all_logs'            => 'العودة إلى جميع السجلات',
+    'create_a_new_log'            => 'اضافة سجل جديد',
+    'date'                        => 'تاريخ',
+    'delete'                      => 'حذف',
+    'delete_confirmation_title'   => 'تم!',
+    'download'                    => 'تحميل',
+    'delete_error_title'          => 'خطأ',
+    'existing_logs'               => 'السجلات الموجودة',
+    'file_name'                   => 'اسم الملف',
+    'file_size'                   => 'حجم الملف',
+    'delete_cancel_title'         => 'It&#039;s تم',
+    'last_modified'               => 'اخر تحديث',
+    'logs'                        => 'السجلات',
+    'preview'                     => 'مراجع',
+    'delete_cancel_message'       => 'لم يتم حذف ملف السجل.',
+    'delete_error_message'        => 'لم يتم حذف ملف السجل.',
+    'delete_confirmation_message' => 'تم حذف ملف السجل.',
+    'log_file_doesnt_exist'       => "ملف السجل غير موجود.",
+];
